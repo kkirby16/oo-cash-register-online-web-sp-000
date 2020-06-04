135ccd02406f10555b4f7add7019faf707e8827d
Kevin Kirby <kkirby16@my.whitworth.edu>
2020-06-04 21:31:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,8 @@ class CashRegister
     @total += (price * quantity)
     quantity.times do @items << title
   end
-  @last_transaction = price * quantity
+  @last_item_price = price
+  @last_item_quantity = quantity
   end
   
   def apply_discount 
@@ -35,7 +36,7 @@ class CashRegister
   end
     
   def void_last_transaction 
-    @total -= @last_transaction
+    @total -= (@last_item_price * @last_item_quantity)
     if @items.empty?
       total = 0.0
     end
